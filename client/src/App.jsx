2ea8fe67697ece8a013bf7d2ad0343b25b7cc113
Garima Singal <garimasingal13@gmail.com>
2025-11-10 10:31:57 +0530

    Login & sign up page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,19 @@
-//rafc
 import React from 'react';
 import Navbar from "./components/Navbar";
 import { Route, Routes, useLocation } from 'react-router-dom';
 import {Toaster} from 'react-hot-toast'
 import Home from "./pages/Home";
 import Footer from './components/Footer';
+import { useAppContext } from './context/AppContext';
+import Login from './components/Login';
 
 const App = () => {
     const isSellerPath = useLocation().pathname.includes("seller");
-
+    const {showUserLogin} = useAppContext();
   return (
     <div>
       {isSellerPath ?null :<Navbar/>}
-
+       {showUserLogin ? <Login/> : null}
       <Toaster />
       <div className={'${isSellerPath ? " " :"px-6 md:px-16 lg:px-24 xl:px-32"}'} />
         <Routes>
